3fbb455ff54e58116e6a2cc16a2e1557e06eeb4b
Sacristan <girts.kesteris@gmail.com>
2020-07-02 13:03:30 +0300

    initial commit

---
+++ EmbedUnityProject/Assets/Rotate.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(Vector3.up * 10 * Time.deltaTime);
+    }
+}
